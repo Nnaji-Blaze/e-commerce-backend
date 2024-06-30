5dd07cd09395c2bd9756950777225d1973f916f5
nnaji-blaze <nnajisunday@zoho.com>
2024-07-01 00:47:56 +0100

    Fixed endpoint issues and database api call issues

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(cors());
 // Environment variables
 const port = process.env.PORT || 4000;
 const baseUrl = process.env.BASE_URL || "http://localhost:4000";
-const mongoURI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/e-commerce";
+const mongoURI = process.env.MONGODB_URI;
 const jwtSecret = process.env.JWT_SECRET || "secret_ecom";
 const password = process.env.DB_PASSWORD || "Flip%231G%21";
 
@@ -151,6 +150,69 @@ app.get("/allproducts", async (req, res) => {
   res.send(products);
 });
 
+app.get("/newcollections", async (req, res) => {
+  let products = await Product.find({});
+  let arr = products.slice(1).slice(-8);
+  console.log("New Collections");
+  res.send(arr);
+});
+app.get("/popularinwomen", async (req, res) => {
+  try {
+    // Fetch products categorized under "women"
+    let products = await Product.find({
+      category: { $regex: "women", $options: "i" },
+    }).limit(4);
+
+    if (products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No products found in women's category" });
+    }
+
+    console.log("Popular In Women:", products);
+    res.json(products);
+  } catch (error) {
+    console.error(
+      "Error fetching popular products in women's category:",
+      error
+    );
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//Create an endpoint for saving the product in cart
+app.post("/addtocart", fetchuser, async (req, res) => {
+  console.log("Add Cart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  userData.cartData[req.body.itemId] += 1;
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Added");
+});
+
+//Create an endpoint for saving the product in cart
+app.post("/removefromcart", fetchuser, async (req, res) => {
+  console.log("Remove Cart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  if (userData.cartData[req.body.itemId] != 0) {
+    userData.cartData[req.body.itemId] -= 1;
+  }
+  await Users.findOneAndUpdate(
+    { _id: req.user.id },
+    { cartData: userData.cartData }
+  );
+  res.send("Removed");
+});
+
+//Create an endpoint for saving the product in cart
+app.post("/getcart", fetchuser, async (req, res) => {
+  console.log("Get Cart");
+  let userData = await Users.findOne({ _id: req.user.id });
+  res.json(userData.cartData);
+});
+
 app.post("/addproduct", async (req, res) => {
   let products = await Product.find({});
   let id = products.length > 0 ? products[products.length - 1].id + 1 : 1;
@@ -164,6 +226,12 @@ app.post("/addproduct", async (req, res) => {
     description: req.body.description,
   });
   await product.save();
+  res.json({ success: true, name: req.body.name });
+});
+
+app.post("/removeproduct", async (req, res) => {
+  const product = await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Removed");
   res.json({ success: true, name: req.body.name });
 });
 
